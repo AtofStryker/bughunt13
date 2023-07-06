2480f14745e89776ca24359af1979f83a75029c0
Bill Glesias <bglesias@gmail.com>
2023-07-06 14:53:06 -0400

    add some tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,19 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('trying out deckstainhelp', () => {
+  it('navigates correctly to the restore a deck review', () => {
+    cy.visit('https://www.deckstainhelp.com/')
+    cy.get('a[title="Restore-A-Deck Wood Stain Review"]:visible').first().click()
+    cy.get('h1').should('include.text', 'Restore-A-Deck Wood Stain Review')
+  })
+
+  it('navigates correctly to the Top rated product review', () => {
+    cy.visit('https://www.deckstainhelp.com/')
+    cy.get('a[title="Find the Best Wood Deck Stain: Top-Rated Products for All Needs"]:visible').first().click()
+    cy.get('h1').should('include.text', 'Find the Best Wood Deck Stain: Top-Rated Products for All Needs')
+  })
+
+  it('navigates correctly to the about deck stain', () => {
+    cy.visit('https://www.deckstainhelp.com/')
+    cy.get('a[href="https://www.deckstainhelp.com/about-deckstainhelp-com/"]:visible').first().click()
+    cy.get('h1').should('include.text', 'About DeckStainHelp.com')
   })
 })
